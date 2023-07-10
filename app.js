90f80eb2022ae99649bea66cf7e9fbd9266a47c8
Raghav Sharma <rag.s318@gmail.com>
2023-07-10 23:04:43 +0530

    hope commit

---
+++ app.js
@@ -0,0 +1,93 @@
+// import express from "express";
+
+// import bodyParser from "body-parser";
+// import request from "request";
+// import path from "path";
+// import { fileURLToPath } from "url";
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("node:https");
+// const path = require("path");
+// const { fileURLToPath } = require("url");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+mailchimp.setConfig({
+  apiKey: "61ba5dd4078e0e69cb1a0bbd8b0a67f3-us21",
+  server: "us21",
+});
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  var FirstName = req.body.fName;
+  var LastName = req.body.lName;
+  var email = req.body.email;
+  var listID = "1d68976b31";
+  // const run = async () => {
+  //   const response = await client.lists.addListMember(listID, {
+  //     email_address: email,
+  //     status: "subscribed",
+  //     merge_fields: {
+  //       FNAME: FirstName,
+  //       LNAME: LastName,
+  //     },
+  //   });
+  //   console.log(response);
+  // };
+  //run();
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: FirstName,
+          LNAME: LastName,
+        },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);
+  const url = "https://us21.api.mailchimp.com/3.0/lists/1d68976b31";
+  const Options = {
+    method: "POST",
+    auth: "raghav1:61ba5dd4078e0e69cb1a0bbd8b0a67f3-us21",
+  };
+  const request = https.request(url, Options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+  //request.write(jsonData);
+  request.end();
+  //console.log(firstName, lastName, email);
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server running on port 3000");
+});
+
+//API
+//61ba5dd4078e0e69cb1a0bbd8b0a67f3-us21
+
+//ID
+//1d68976b31
