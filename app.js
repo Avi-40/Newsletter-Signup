087b338093c4e6a31a9eec0b63db79a6fe6b75e9
Raghav Sharma <83939560+Avi-40@users.noreply.github.com>
2023-07-31 16:50:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ mailchimp.setConfig({
   apiKey: `${process.env.API_KEY}`,
   server: "us21",
 });
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
